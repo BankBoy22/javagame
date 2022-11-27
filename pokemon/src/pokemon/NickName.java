414d2361ef758e595d6e07ebaeabdbabf825ea91
Berryl1 <117261796+Berryl1@users.noreply.github.com>
2022-11-27 18:51:06 +0900

    이거 화면 중간에 넣고 싶은데 텍스트 상자는 어케하는지 몰라서 일단 함

---
+++ pokemon/src/pokemon/NickName.java
@@ -0,0 +1,58 @@
+
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class NickName extends JFrame{
+	
+	int WIDTH=1200;
+	int HEIGHT=800;
+	
+	JPanel p = new JPanel();
+	Font font = new Font("HY견명조",Font.BOLD,30);
+	
+	public void go() {
+		setTitle("Pokemon");
+		setSize(WIDTH,HEIGHT);
+		
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		JLabel l1 = new JLabel("나의 이름 : ", JLabel.CENTER);
+		JLabel l2 = new JLabel("상대방의 이름 : ", JLabel.RIGHT);
+		
+		l1.setFont(font);
+		l2.setFont(font);
+		
+		
+		JTextField tf1 = new JTextField(8);
+		JTextField tf2 = new JTextField(8);
+		
+		tf1.setBounds(50,70,700,700);
+		tf1.setFont(font);
+		
+		tf2.setBounds(50,70,700,700);
+		tf2.setFont(font);
+		
+		
+		add(p);
+		p.add(l1);
+		p.add(tf1);
+		
+		p.add(l2);
+		p.add(tf2);
+		
+		l1.setLayout(new GridLayout(3,1));
+		
+		
+		
+		
+		
+		
+		setVisible(true);
+	}
+}
---
+++ pokemon/src/pokemon/NickNameDriver.java
@@ -0,0 +1,10 @@
+
+public class NickNameDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NickName nick = new NickName();
+		nick.go();
+	}
+
+}
